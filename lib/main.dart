254035ced848d16844a4a776a6a1eba34874bb4c
Adharsh Reghu <adharshreghu2002@gmail.com>
2023-07-01 20:40:22 +0530

    email authentication

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,12 @@
 import 'package:busmate/Screens/login.dart';
 import 'package:busmate/Screens/ticket_details.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
-void main() {
+Future main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(const MyApp());
 }
 
